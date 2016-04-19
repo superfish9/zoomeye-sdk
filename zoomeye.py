5364e600d040f0c4b71492a8e2c0a383de9cd58d
superfish9 <sfish9@163.com>
2016-04-19 13:41:10 +0800

    Update zoomeye.py

--- zoomeye.py
+++ zoomeye.py
@@ -23,9 +23,15 @@ class ZoomeyeError(Exception):
 
 class ZoomeyeResult(object):
 	'''
-	Make result a class
+	Make result a class.
 	'''
 	def __init__(self, result):
+		self.plan = ''
+		self.resources = ''
+		self.matches = ''
+		self.facets = ''
+		self.total = ''
+
 		result = json.loads(result)
 		if result.has_key('plan'):
 			self.plan = result['plan']
@@ -33,10 +39,88 @@ class ZoomeyeResult(object):
 			self.resources = result['resources']
 		if result.has_key('matches'):
 			self.matches = result['matches']
+			self.result_len = len(self.matches)
 		if result.has_key('facets'):
 			self.facets = result['facets']
 		if result.has_key('total'):
 			self.total = result['total']
+
+	def get_ip_list(self, num=0):
+		'''
+		Get ip list from result. Only for host search.
+		'''
+		if not self.matches or not isinstance(self.matches[0]['ip'], basestring):
+			return []
+
+		ip_list = []
+		if num <= 0 or num >= self.result_len:
+			ip_num = self.result_len
+		else:
+			ip_num = num
+		for i in range(0, ip_num):
+			ip_list.append(self.matches[i]['ip'])
+		return ip_list
+
+	def get_portinfo_list(self, ip=[]):
+		'''
+		Get portinfo list (of some ip) from result. Only for host search.
+		'''
+		if not self.matches or not self.matches[0].has_key('portinfo'):
+			return {}
+
+		portinfo_list = {}
+		if isinstance(ip, basestring):
+			ip_list = ip
+		elif ip == []:
+			ip_list = self.get_ip_list()
+		else:
+			ip_list = ip
+		for i in range(0, self.result_len):
+			if self.matches[i]['ip'] in ip_list:
+				portinfo_list[self.matches[i]['ip']] = self.matches[i]['portinfo']
+		return portinfo_list
+
+	def get_site_list(self, num=0):
+		'''
+		Get site list from result. Only for Web search.
+		'''
+		if not self.matches or not self.matches[0].has_key('site'):
+			return []
+
+		site_list = []
+		if num <= 0 or num >= self.result_len:
+			site_num = self.result_len
+		else:
+			site_num = num
+		for i in range(0, site_num):
+			site_list.append(self.matches[i]['site'])
+		return site_list
+
+	def get_webinfo_list(self, site=[]):
+		'''
+		Get webinfo list (of some site) from result. Only for Web search.
+		'''
+		if not self.matches or not self.matches[0].has_key('site'):
+			return {}
+
+		webinfo_list = {}
+		if isinstance(site, basestring):
+			site_list = site
+		elif site == []:
+			site_list = self.get_site_list()
+		else:
+			site_list = site
+		for i in range(0, self.result_len):
+			if self.matches[i]['site'] in site_list:
+				webinfo_list[self.matches[i]['site']] = {}
+				webinfo_list[self.matches[i]['site']]['db'] = self.matches[i]['db']
+				webinfo_list[self.matches[i]['site']]['domains'] = self.matches[i]['domains']
+				webinfo_list[self.matches[i]['site']]['language'] = self.matches[i]['language']
+				webinfo_list[self.matches[i]['site']]['ip'] = self.matches[i]['ip']
+				#webinfo_list[self.matches[i]['site']]['server'] = self.matches[i]['server']
+				webinfo_list[self.matches[i]['site']]['webapp'] = self.matches[i]['webapp']
+		return webinfo_list
+
 
 class zoomeye(object):
 	'''
